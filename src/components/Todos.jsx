7a4623fe9c939f07d481d44a1da419a2afea4d29
Anggriawan Rayzadmiko <anggriawan.net@gmail.com>
2024-06-05 21:41:15 +0700

    Develop toggle todo feature

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,11 +1,11 @@
 import TodoItem from './TodoItem';
 
-function Todos({ todos }) {
+function Todos({ todos, toggleCompleted }) {
   return (
     <div className="flex flex-col gap-6">
       {todos.map((todo) => (
         // <p key={todo.id}>{todo.title}</p>
-        <TodoItem key={todo.id} todo={todo} />
+        <TodoItem key={todo.id} todo={todo} toggleCompleted={toggleCompleted} />
       ))}
     </div>
   );
