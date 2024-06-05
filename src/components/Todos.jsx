4bded261a553666bde22389c152400a6621f6670
Anggriawan Rayzadmiko <anggriawan.net@gmail.com>
2024-06-05 20:53:40 +0700

    Add styling to components

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -2,7 +2,7 @@ import TodoItem from './TodoItem';
 
 function Todos({ todos }) {
   return (
-    <div>
+    <div className="flex flex-col gap-6">
       {todos.map((todo) => (
         // <p key={todo.id}>{todo.title}</p>
         <TodoItem key={todo.id} todo={todo} />
